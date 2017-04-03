d47c717e802c795e06b5f84fe9bd44484967c6c8
pdevkota1 <prabesh.devkota@gmail.com>
2017-04-03 09:15:43 -0700

    First commit

---
+++ comparer_app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify, make_response, render_template
+from json_comparer.dict_comparer import comparer
+import json
+import sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('json_comparer.html')
+
+
+@app.route('/api/v1.0/compare_json', methods=['POST'])
+def compare_json():
+	print("data is %s" % request.json)
+	data = request.json
+	try:
+		dict1 = json.loads(data["first_json"])
+	except Exception as e: 
+		return jsonify(error=403, text="First JSON Invalid!!"), 403
+	try:
+		dict2 = json.loads(data["second_json"])
+	except Exception as e:
+		return jsonify(error=403, text="Second json Invalid!!"), 403
+	res = comparer(dict1, dict2, dict1_name="first_json", dict2_name="second_json")
+	return jsonify({"diff": res})
